fc0044a0ccc7591217e789c3ca6dbd6da34c9017
codlife <1004910847@qq.com>
2016-09-26 10:42:42 +0800

    fix client

--- src/main/scala/org/apache/spark/asyspark/core/Client.scala
+++ src/main/scala/org/apache/spark/asyspark/core/Client.scala
@@ -4,17 +4,19 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props, _}
 import akka.pattern.ask
+import akka.remote.RemoteScope
 import akka.util.Timeout
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.slf4j.StrictLogging
-import org.apache.spark.asyspark.core.messages.master.RegisterClient
+import org.apache.spark.asyspark.core.messages.master.{RegisterClient, ServerList}
 import org.apache.spark.asyspark.core.models.client.BigVector
+import org.apache.spark.asyspark.core.models.client.asyImp.{AsyBigVectorDouble, AsyBigVectorFloat, AsyBigVectorInt, AsyBigVectorLong}
 import org.apache.spark.asyspark.core.models.server.{PartialVectorDouble, PartialVectorFloat, PartialVectorInt, PartialVectorLong}
 import org.apache.spark.asyspark.core.partitions.range.RangePartitioner
 import org.apache.spark.asyspark.core.partitions.{Partition, Partitioner}
 
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.reflect.runtime.universe.{TypeTag, typeOf}
 /**
   * The client is to spawn a large distributed model such as BigVector on parameter servers
@@ -29,6 +31,61 @@ class Client(val config: Config, private[asyspark] val system: ActorSystem,
   private[asyspark] val registration = master ? RegisterClient(actor)
 
 
+
+
+  /**
+    * Creates a distributed model on the parameter servers
+    *
+    * @param keys The total number of keys
+    * @param modelsPerServer The number of models to spawn per parameter server
+    * @param createPartitioner A function that creates a partitioner based on a number of keys and partitions
+    * @param generateServerProp A function that generates a server prop of a partial model for a particular partition
+    * @param generateClientObject A function that generates a client object based on the partitioner and spawned models
+    * @tparam M The final model type to generate
+    * @return The generated model
+    */
+  private def create[M](keys: Long,
+                        modelsPerServer: Int,
+                        createPartitioner: (Int, Long) => Partitioner,
+                        generateServerProp: Partition => Props,
+                        generateClientObject: (Partitioner, Array[ActorRef], Config) => M): M = {
+
+    // Get a list of servers
+    val listOfServers = serverList()
+
+    // Construct a big model based on the list of servers
+    val bigModelFuture = listOfServers.map { servers =>
+
+      // Check if there are servers online
+      if (servers.isEmpty) {
+        throw new Exception("Cannot create a model without active parameter servers")
+      }
+
+      // Construct a partitioner
+      val numberOfPartitions = Math.min(keys, modelsPerServer * servers.length).toInt
+      val partitioner = createPartitioner(numberOfPartitions, keys)
+      val partitions = partitioner.all()
+
+      // Spawn models that are deployed on the parameter servers according to the partitioner
+      val models = new Array[ActorRef](numberOfPartitions)
+      var partitionIndex = 0
+      while (partitionIndex < numberOfPartitions) {
+        val serverIndex = partitionIndex % servers.length
+        val server = servers(serverIndex)
+        val partition = partitions(partitionIndex)
+        val prop = generateServerProp(partition)
+        models(partitionIndex) = system.actorOf(prop.withDeploy(Deploy(scope = RemoteScope(server.path.address))))
+        partitionIndex += 1
+      }
+
+      // Construct a big model client object
+      generateClientObject(partitioner, models, config)
+    }
+
+    // Wait for the big model to finish
+    Await.result(bigModelFuture, config.getDuration("asyspark.client.timeout", TimeUnit.MILLISECONDS) milliseconds)
+
+  }
   def bigVector[V: breeze.math.Semiring: TypeTag](keys: Long, modelsPerServer: Int = 1): BigVector[V] = {
     bigVector[V](keys, modelsPerServer, (partitions: Int, keys: Long) => RangePartitioner(partitions, keys))
   }
@@ -45,6 +102,25 @@ class Client(val config: Config, private[asyspark] val system: ActorSystem,
 
     }
 
+    val objFunction = numberType[V] match {
+      case "Int" => (partitioner: Partitioner, models: Array[ActorRef], config: Config) =>
+        new AsyBigVectorInt(partitioner, models, config, keys).asInstanceOf[BigVector[V]]
+      case "Long" => (partitioner: Partitioner, models: Array[ActorRef], config: Config) =>
+        new AsyBigVectorLong(partitioner, models, config, keys).asInstanceOf[BigVector[V]]
+      case "Float" => (partitioner: Partitioner, models: Array[ActorRef], config: Config) =>
+        new AsyBigVectorFloat(partitioner, models, config, keys).asInstanceOf[BigVector[V]]
+      case "Double" => (partitioner: Partitioner, models: Array[ActorRef], config: Config) =>
+        new AsyBigVectorDouble(partitioner, models, config, keys).asInstanceOf[BigVector[V]]
+      case x => throw new Exception(s"Cannot create model for unsupported value type $x")
+    }
+    create[BigVector[V]](keys, modelsPerServer, createPartitioner, propFunction, objFunction)
+
+
+
+  }
+
+  def serverList(): Future[Array[ActorRef]] = {
+    (master ? ServerList).mapTo[Array[ActorRef]]
   }
 
   def numberType[V: TypeTag]: String = {
@@ -65,8 +141,98 @@ class Client(val config: Config, private[asyspark] val system: ActorSystem,
   }
 
 }
+
+/**
+  * Contains functions to easily create a client object that is connected to the asyspark cluster.
+  *
+  * You can construct a client with a specific configuration:
+  * {{{
+  *   import asyspark.Client
+  *
+  *   import java.io.File
+  *   import com.typesafe.config.ConfigFactory
+  *
+  *   val config = ConfigFactory.parseFile(new File("/your/file.conf"))
+  *   val client = Client(config)
+  * }}}
+  *
+  * The resulting client object can then be used to create distributed matrices or vectors on the available parameter
+  * servers:
+  * {{{
+  *   val matrix = client.matrix[Double](10000, 50)
+  * }}}
+  */
+object Client {
+
+  /**
+    * Constructs a client with the default configuration
+    *
+    * @return The client
+    */
+  def apply(): Client = {
+    this(ConfigFactory.empty())
+  }
+
+  /**
+    * Constructs a client
+    *
+    * @param config The configuration
+    * @return A future Client
+    */
+  def apply(config: Config): Client = {
+    val default = ConfigFactory.parseResourcesAnySyntax("asyspark")
+    val conf = config.withFallback(default).resolve()
+    Await.result(start(conf), conf.getDuration("asyspark.client.timeout", TimeUnit.MILLISECONDS) milliseconds)
+  }
+
+  /**
+    * Implementation to start a client by constructing an ActorSystem and establishing a connection to a master. It
+    * creates the Client object and checks if its registration actually succeeds
+    *
+    * @param config The configuration
+    * @return The future client
+    */
+  private def start(config: Config): Future[Client] = {
+
+    // Get information from config
+    val masterHost = config.getString("asyspark.master.host")
+    val masterPort = config.getInt("asyspark.master.port")
+    val masterName = config.getString("asyspark.master.name")
+    val masterSystem = config.getString("asyspark.master.system")
+
+    // Construct system and reference to master
+    val system = ActorSystem(config.getString("asyspark.client.system"), config.getConfig("asyspark.client"))
+    val master = system.actorSelection(s"akka.tcp://${masterSystem}@${masterHost}:${masterPort}/user/${masterName}")
+
+    // Set up implicit values for concurrency
+    implicit val ec = ExecutionContext.Implicits.global
+    implicit val timeout = Timeout(config.getDuration("asyspark.client.timeout", TimeUnit.MILLISECONDS) milliseconds)
+
+    // Resolve master node asynchronously
+    val masterFuture = master.resolveOne()
+
+    // Construct client based on resolved master asynchronously
+    masterFuture.flatMap {
+      case m =>
+        val client = new Client(config, system, m)
+        client.registration.map {
+          case true => client
+          case _ => throw new RuntimeException("Invalid client registration response from master")
+        }
+    }
+  }
+
+
+}
+
+/**
+  * The client actor class. The master keeps a death watch on this actor and knows when it is terminated.
+  *
+  * This actor either gets terminated when the system shuts down (e.g. when the Client object is destroyed) or when it
+  * crashes unexpectedly.
+  */
 private class ClientActor extends Actor with ActorLogging {
   override def receive: Receive = {
-    case x => log.info(s"Client actor reveiced message ${x}")
+    case x => log.info(s"Client actor received message ${x}")
   }
 }
